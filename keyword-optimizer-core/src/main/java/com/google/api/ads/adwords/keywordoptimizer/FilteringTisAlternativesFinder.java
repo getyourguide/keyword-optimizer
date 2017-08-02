9a5f51acceb88cdd1af4442982227e5187434316
Jared Niederhauser <jared@getyourguide.com>
2017-08-02 16:58:36 +0200

    Focus day

---
+++ keyword-optimizer-core/src/main/java/com/google/api/ads/adwords/keywordoptimizer/FilteringTisAlternativesFinder.java
@@ -0,0 +1,30 @@
+package com.google.api.ads.adwords.keywordoptimizer;
+
+/**
+ * Created by jarednieder on 8/2/2017.
+ */
+public class FilteringTisAlternativesFinder extends TisAlternativesFinder {
+    private final String filterString;
+
+    /**
+     * Creates a new {@link FilteringTisAlternativesFinder}.
+     *
+     * @param context holding shared objects during the optimization process
+     */
+    public FilteringTisAlternativesFinder(OptimizationContext context) {
+        super(context);
+        this.filterString = "eiffel tower";
+    }
+
+    @Override
+    public KeywordCollection derive(KeywordCollection keywords) throws KeywordOptimizerException {
+        KeywordCollection results = super.derive(keywords);
+        KeywordCollection filteredResults = new KeywordCollection(results.getCampaignConfiguration());
+        for (KeywordInfo kwInfo : results) {
+            if (kwInfo.getKeyword().getText().equals(filterString)) {
+                filteredResults.add(kwInfo);
+            }
+        }
+        return filteredResults;
+    }
+}
